e14f803cf9901b09bfaab09e69b2a53bd2d49cbf
BLoushi52 <k.albloushii@hotmail.com>
2022-11-20 17:48:42 +0300

    removed unnecessary stuff

--- lib/models/food.dart
+++ lib/models/food.dart
@@ -16,16 +16,3 @@ List<Food> foods = [
   Food(name: "Pasta", Imgpath: "assets/pasta.jpg"),
   Food(name: "Sushi", Imgpath: "assets/sushi.jpg"),
 ];
-
-List imgPaths = [
-  'assets/toy_story.jpeg',
-  'assets/spider_man.jpeg',
-  'assets/inside_out.jpeg',
-  'assets/lego.jpeg',
-  'assets/lion_king.jpeg',
-  'assets/frozen.jpeg',
-  'assets/shrek.jpeg',
-  'assets/big_hero.jpeg',
-  'assets/ice_age.jpeg',
-  'assets/brave.jpeg',
-];
